1c12f4a18b70dee316c2451255a73b473dcbc33d
kafka <a50541853@gmail.com>
2018-10-07 13:20:33 +0800

    upload

---
+++ ch_1/G_change_L.java
@@ -0,0 +1,14 @@
+public class G_change_L {
+  public static void main (String args[]) {
+    double l;
+    double g;
+    double coe_g_to_l;
+    coe_g_to_l = 3.7845;
+    g = 10;
+    System.out.println("the 10 gallon is eval"+ "  " +  g*coe_g_to_l  +" " +"litery");
+
+  }
+}
+
+
+
