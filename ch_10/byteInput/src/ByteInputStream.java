c24e596dd4d7702504091836ba6e4ec8ae330fcf
kafka <a50541853@gmail.com>
2018-12-01 18:38:42 +0800

    注释小修补

--- ch_10/byteInput/src/ByteInputStream.java
+++ ch_10/byteInput/src/ByteInputStream.java
@@ -7,7 +7,7 @@ public class ByteInputStream {
     System.in.read(data);//将字节数据读取到data数组中
     System.out.print("you enter : ");
     for (int i = 0; i < data.length; i++)
-      System.out.print((char) data[i]);//由结果可以得出一个字符(包括空格)占据一个数组位,而每一个数组为只存一个字节
+      System.out.print((char) data[i]);//由结果可以得出一个字符(包括空格)占据一个数组位,而每一个数组位只存一个字节
   }
 
 }
