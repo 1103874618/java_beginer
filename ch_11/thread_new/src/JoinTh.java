fd92b2083611562735da03bde1443d9d001beeec
kafka <a50541853@gmail.com>
2019-01-08 16:41:34 +0800

    join 使用

---
+++ ch_11/thread_new/src/JoinTh.java
@@ -0,0 +1,28 @@
+public class JoinTh {
+  public static void main(String args[]){
+    System.out.println("Main thread starting .");
+    MytheadIm mt1 = new MytheadIm(("Child_1"));
+    MytheadIm mt2 = new MytheadIm(("Child_2"));
+    MytheadIm mt3 = new MytheadIm(("Child_3"));
+//这里对象一创建就可以执行线程
+
+      System.out.print(".");
+      try {
+        mt1.thrd.join();
+        System.out.println(mt1.thrd.getName()+ "Join .");
+
+        mt2.thrd.join();
+        System.out.println(mt2.thrd.getName()+ "Join .");
+
+        mt2.thrd.join();
+        System.out.println(mt2.thrd.getName()+ "Join .");
+      }catch (InterruptedException e){
+        System.out.println("main thread interruptiuted");
+      }
+//    }while (mt1.thrd.isAlive() ||
+//            mt2.thrd.isAlive() ||
+//            mt3.thrd.isAlive());//利用isAlive来判断主线程是否需要结束
+
+    System.out.println("Main thread ending");
+  }
+}
